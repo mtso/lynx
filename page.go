491f77625fad8eee69fa734f2f953bcb2112abef
mtso <mtso@tuta.io>
2016-12-29 00:23:19 -0800

    Implement page.ExportTo() to 'public' dir

--- page.go
+++ page.go
@@ -2,12 +2,9 @@ package lynx
 
 import (
 	"log"
-	// "errors"
 	"path/filepath"
-	// "bufio"
-	// "io"
 	"io/ioutil"
-	// "os"
+	"os"
 	"html/template"
 )
 
@@ -31,21 +28,25 @@ type Page struct {
 type PageGroup []Page
 
 func NewPage(t string, n *Page, c string) *Page {
-	return &Page {
+	return &Page{
 		Title: t,
-		Next: n,
+		Next:  n,
 		// Link: l,
 		Content: c,
-		html: make([]byte, 0),
+		html:    make([]byte, 0),
 	}
 }
 
 // Implement Writer interface
 func (p *Page) Write(in []byte) (n int, err error) {
-	for _, b := range in {
-		p.html = append(p.html, b)
-	}
+	p.html = append(p.html, in...)
 	return len(in), nil
+}
+
+// Implement Reader interface
+func (p *Page) Read(out []byte) (n int, err error) {
+	out = append(out, p.html...)
+	return len(p.html), nil
 }
 
 func LoadPagesIn(dirname string) PageGroup {
@@ -68,7 +69,6 @@ func LoadPagesIn(dirname string) PageGroup {
 			continue
 		}
 
-		// content := string(buf)
 		content := string(buf[:len(buf)])
 		newpage := NewPage(file.Name(), prev, content)
 		prev = newpage
@@ -81,17 +81,26 @@ func LoadPagesIn(dirname string) PageGroup {
 
 func (pages PageGroup) ExportTo(dirname string) error {
 
+	// Init a new template by parsing post-demo file
 	t, err := template.ParseFiles("template/post-demo.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, p := range pages {
+		// Execute post-demo template with Page object
 		err = t.Execute(&p, p)
 		if err != nil {
 			log.Println(err)
+			continue
 		}
 
+		filepath := filepath.Join("public", p.Title) + ".html"
+		err = ioutil.WriteFile(filepath, p.html, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 	}
 
 	return err
