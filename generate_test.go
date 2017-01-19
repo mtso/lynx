72d721228a63f5deaae97df7f1ade17dd4eea952
mtso <mtso@tuta.io>
2017-01-18 19:16:56 -0800

    Add simple tests for generate.go

--- generate_test.go
+++ generate_test.go
@@ -5,12 +5,33 @@ import (
 	"os"
 )
 
+func Test_generateDefault(t *testing.T) {
+	testDir := defaultExportDir
+	Generate()
+	assertExists(testDir, t)
+	_ = os.RemoveAll(testDir)
+}
+
+func Test_generateWith(t *testing.T) {
+	testDir := "./generate_test"
+	testconfig := &Configuration{
+		Title: "Generation Test Title",
+		Description: "Generation Test Description",
+		ExportDir: &testDir,
+	}
+	GenerateWith(*testconfig)
+	assertExists(testDir, t)
+	_ = os.RemoveAll(testDir)
+}
+
 func Test_mkdirIfNone(t *testing.T) {
 	testDir := "./test"
-	_ = os.RemoveAll(testDir)
 	err := mkdirIfNone(testDir)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
 	assertExists(testDir, t)
+
+	// Clean test directories
+	_ = os.RemoveAll(testDir)
 }
