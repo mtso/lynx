43b5d5eb215bce1e7ec734b1d5114d36b474bba4
mtso <mtso@tuta.io>
2017-01-23 10:09:41 -0800

    Stub LynxTemplate type

--- cmd/pack/template_info.go
+++ cmd/pack/template_info.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"html/template"
+)
+
+const (
+	INDEX = iota
+	POST
+)
+
 type templateType int
 
 type templateInfo struct {
 	templatetype templateType
 	data         string
+}
+
+type LynxTemplate struct {
+	Templates map[templateType]template.Template
 }
